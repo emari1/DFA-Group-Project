712e0f8c1a13d74adf57995ece577695f5eeabf3
elijahmari <emari1@students.towson.edu>
2026-03-22 20:40:38 -0400

    done

---
+++ src/States.java
@@ -0,0 +1,13 @@
+import com.sun.source.tree.NewArrayTree;
+
+public class States {
+    private int oldState;
+    private int symbol;
+    private int newState;
+
+    public States(int oldState, int symbol, int newState){
+        this.oldState = oldState;
+        this.symbol = symbol;
+        this.newState = newState;
+    }
+}
