c5219da8f2c8c9d55e1cfc8a08df938d59e2f3ae
elijahmari <emari1@students.towson.edu>
2026-03-22 20:49:57 -0400

    added more

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        List<String> acceptStates;
 
+        BufferedReader reader = new BufferedReader(new FileReader("/home/elijahmari/IdeaProjects/DFA/src/DFAFile"));
+        System.out.println(reader);
     }
 }
