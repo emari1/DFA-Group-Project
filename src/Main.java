15ea68b6901a315a4c8cfbb5fc4fae6443caf577
elijahmari <emari1@students.towson.edu>
2026-03-22 21:09:23 -0400

    simple files reading shit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -11,14 +12,23 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        List<String> acceptStates;
-
+        List<String> readingFromFile = new ArrayList<>();
+        List<String> acceptStates = new ArrayList<>();
+        String numberOfStates;
         BufferedReader reader = new BufferedReader(new FileReader("/home/elijahmari/IdeaProjects/DFA/src/DFAFile"));
         String line;
         while((line = reader.readLine()) != null){
             System.out.println(line);
-
+            readingFromFile.add(line);
         }
+        System.out.println(readingFromFile.get(1));
+        numberOfStates = readingFromFile.get(0);
+        acceptStates.add(readingFromFile.get(1));
+
+        for(int i = 0; i < Integer.parseInt(numberOfStates); i++){
+            
+        }
+
 
 
     }
