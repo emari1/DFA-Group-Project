1b3f4a7d7ab51fd7de626aa1853a87ba91eb1300
elijahmari <emari1@students.towson.edu>
2026-03-24 19:49:32 -0400

    added more stuff for input and reading from file

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
+//Elijah Mari, Maxwell Fadley, Kelvin Okocha, Edward Deensie
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -20,28 +21,35 @@ public class Main {
         String numberOfStates;
         BufferedReader reader = new BufferedReader(new FileReader("/home/elijahmari/IdeaProjects/DFA/src/DFAFile"));
         String line;
-        while((line = reader.readLine()) != null){
-            System.out.println(line);
-            readingFromFile.add(line);
+        int whole=reader.read();
+        System.out.println(whole);
+        Scanner scanner = new Scanner(System.in);
+        if(whole  != -1){
+            System.out.println("input the string you would like to use");
+            String userInput=scanner.nextLine();
+            System.out.println(userInput);
+            while((line = reader.readLine()) != null){
+                System.out.println(line);
+                readingFromFile.add(line);
+            }
+
+            System.out.println(readingFromFile.get(1));
+            numberOfStates = readingFromFile.get(0);
+            String acception = readingFromFile.get(1);
+            String [] arr = acception.split(" ");
+            acceptStates.addAll(Arrays.asList(arr));
+            System.out.println(acceptStates.get(0));
+
+
+//            for(int i = 2; i < Integer.parseInt(numberOfStates); i++){
+//                //make the States objects
+//                States state = new States(0,0,0);
+//                statesWTransitions.add(state);
+//            }
+
         }
-
-        System.out.println(readingFromFile.get(1));
-        numberOfStates = readingFromFile.get(0);
-        String acception = readingFromFile.get(1);
-        String [] arr = acception.split(" ");
-        acceptStates.addAll(Arrays.asList(arr));
-        System.out.println(acceptStates.get(0));
-
-
-        for(int i = 2; i < Integer.parseInt(numberOfStates); i++){
-            //make the States objects
-            States state = new States(0,0,0);
-            statesWTransitions.add(state);
+        else {
+            throw new IOException("File has length of zero");
         }
-
-
-
-
-
     }
 }
